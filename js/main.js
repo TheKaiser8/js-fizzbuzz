a40c0b03cc81bef60f14597333a64c8fea2e8ffd
TheKaiser8 <stefano.vanzini8@gmail.com>
2022-10-24 15:58:30 +0200

    Add commento procedimento esercizio

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
 "use strict";
+// Consegna:
+// Scrivi un programma che stampi in console i numeri da 1 a 100, ma che per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi “Buzz”. Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
+
+// 1. Creo un ciclo for che mi generi i numeri da 1 a 100
+// 2. Stampo in console i numeri del ciclo for
+// 3. Determino con un'istruzione condizionale all'interno del ciclo for i numeri multipli di 3,5 e di entrambi
+//    3a. SE SOLO multipli di 3 stampare "Fizz" in console
+//        ALTRIMENTI SE SOLO multipli di 5 stampare "Buzz" in console
+//        ALTRIMENTI SE multipli sia di 3 che di 5 stampare "FizzBuzz in console".
